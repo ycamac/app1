7208f4fca4dd9777d954b69b972e8df619b9d0d5
ycamac <y.cama@outlook.com>
2025-07-23 09:26:57 -0600

    loading starter files

---
+++ radio_button_sidebar.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+
+st.sidebar.write("Type of Analysis")
+selected_option=st.sidebar.radio("Select any option", options=("Line Graph","Bar Graph","Area Chart"))
+ 
+data = pd.DataFrame(np.random.randn(50,2), columns=['Age','BMI'])
+ 
+if selected_option=='Line Graph':
+    st.line_chart(data)
+elif selected_option=='Bar Graph':
+    st.bar_chart(data)
+else:
+    st.area_chart(data)
